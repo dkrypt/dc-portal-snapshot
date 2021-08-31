9ef1b40a379d326233708c993bb138074f213807
GUPTA <kavita.gupta@capgemini.com>
2021-08-31 17:13:12 +0530

    Globalscape api integration

--- src/containers/globalScape/api/api.js
+++ src/containers/globalScape/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  const AxiosInstance = axios.create({
-    baseURL: 'https://tc-eng-portal-aws.digitalconnect.apps.ge.com/api',
-    timeout: 1000
+    baseURL: 'https://tc-eng-portal-aws.digitalconnect.apps.ge.com/api/02/SelfServicePortal',
+    timeout: 100000
   });
 
   export default AxiosInstance;
